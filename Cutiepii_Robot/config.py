9ee1c3b4a249bd07bf92da460be599e11082288c
erenop2k <105239864+erenop2k@users.noreply.github.com>
2022-09-13 18:56:30 +0530

    Update config.py

--- Cutiepii_Robot/config.py
+++ Cutiepii_Robot/config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/CutiepiiRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/Cutiepii_Robot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
